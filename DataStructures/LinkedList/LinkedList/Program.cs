6a44e74504c2dea9871dc363a5567c4eb4ab96af
Michael Kelly <mike.s.kelly@icloud.com>
2019-03-25 09:31:13 -0700

    Initial

---
+++ DataStructures/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void instantiateList()
+        {
+            LinkedList<int> numbers = new LinkedList<int>();
+        }
+    }
+}
---
+++ DataStructures/LinkedList/LinkedList_TDD/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using LinkedList.Program;
+
+namespace LinkedList_TDD
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
