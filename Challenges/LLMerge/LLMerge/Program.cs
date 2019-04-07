74c8f21388308e41790f6588dfaa7ecb112ef4b3
Michael Kelly <mike.s.kelly@icloud.com>
2019-04-06 23:38:18 -0700

    Working on first test

---
+++ Challenges/LLMerge/LLMerge/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using LLMerge.Classes;
+
+namespace LLMerge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Merges two Linked Lists into one Linked List
+        /// </summary>
+        /// <param name="list1">First Linked List to be merged</param>
+        /// <param name="list2">Second Linked List to be merged</param>
+        /// <returns>Merged Linked List</returns>
+        public static SLinkedList MergeTwoLL(SLinkedList list1, SLinkedList list2)
+        {
+            int length1 = list1.LLLength();
+            int length2 = list2.LLLength();
+
+            if (length2 > length1)
+            {
+                list2.Head = list2.Current;
+                list1.Head = list1.Current;
+
+                SLinkedList list = new SLinkedList(list2.Current);
+                list.Insert(list1.Current.Data);
+
+                list2.Current = list2.Current.Next;
+                list1.Current = list1.Current.Next;
+
+
+                while (list2.Current.Next != null && list1.Current.Next != null)
+                {
+                    list.Insert(list2.Current.Data);
+                    list.Insert(list1.Current.Data);
+                    list2.Current = list2.Current.Next;
+                    list1.Current = list1.Current.Next;
+                }
+                if (list2.Current.Next != null)
+                {
+                    while (list2.Current.Next != null)
+                    {
+                        list.Insert(list2.Current.Data);
+                        list2.Current = list2.Current.Next;
+                    }
+                }
+                list.Insert(list2.Current.Data);
+                list.Insert(list1.Current.Data);
+
+                return list;
+            }
+            else
+            {
+                list1.Head = list1.Current;
+                list2.Head = list2.Current;
+
+                SLinkedList list = new SLinkedList(list1.Current);
+                list.Insert(list1.Current.Data);
+
+                list1.Current = list1.Current.Next;
+                list2.Current = list2.Current.Next;
+
+
+                while (list1.Current.Next != null && list2.Current.Next != null)
+                {
+                    list.Insert(list1.Current.Data);
+                    list.Insert(list2.Current.Data);
+                    list1.Current = list1.Current.Next;
+                    list2.Current = list2.Current.Next;
+                }
+                if (list1.Current.Next != null)
+                {
+                    while (list1.Current.Next != null)
+                    {
+                        list.Insert(list1.Current.Data);
+                        list1.Current = list1.Current.Next;
+                    }
+                }
+                list.Insert(list1.Current.Data);
+                list.Insert(list2.Current.Data);
+                return list;
+            }
+
+        }
+
+    }
+}
---
+++ Challenges/LLMerge/ReadMe.md
@@ -0,0 +1,46 @@
+# Code: Implement a Link Lis***
+------------------------------
+
+# Lab 05: Linked Lists
+#### *Author: Mike Kelly*
+
+------------------------------
+
+## Description
+
+This C# implementation runs through the data-structures of Linked Lists.  This includes establishing a linked list, inserting a node at the head, inserting a node at the end, inserting a node before a specific node, inserting a node after a specific node, and merging 2 nodes.
+
+------------------------------
+
+## Methods
+
+| Method | Summary | Big O Time | Big O Space | Example | 
+| :----------- | :----------- | :-------------: | :-------------: | :----------- |
+| instantiateList | This method creates a new Linked list with 2 nodes. | O(1) | O(1) | N/A |
+| Node | This method creates a new node with inputted data| O(1) | O(1) | Data = 5 |
+| SLinkedList | Required at the creation of a Linked List.  Adds a node assigned to Head.| O(1) | O(1) | Data = 5 |
+| Print | This method prints out a list of integers that are in the Linked List| O(n) | O(1) | 5,4,3,2,1 |
+| Includes | This method checks to see if a praticular integer is in the Linked list| O(n) | O(1) | 3 |
+| Insert | This method inserts a node into the Linked List| O(1) | O(1) | 3 |
+| Append | Appends a new node to the end of the Linked List | O(n) | O(1) | 3 |
+| InsertBefore | Inserts a new node int thte list before a given node | O(n) | O(1) | 3 |
+| InsertAfter | Inserts a new node into the list after a given node | O(n) | O(1) | 3 |
+| ValueAtIndex | Given an index number, returns the value at the Node at that index number | O(n) | O(1) | 3 |
+
+
+------------------------------
+
+## Visuals
+![Image 1](Assets/Chal06Whiteboard.jpg)
+##### BracketValidation
+* Instantiates a Linked List, creates a node, checks to see if a praticular node value is in the linked list, inserts a value in a new node at the head of the linked list, and prints a list of the values in the linked list.
+![Image 1](Assets/PrintMethod.PNG)
+
+------------------------------
+
+## Change Log
+1.1 Updated the ReadMe file
+
+------------------------------
+
+For more information on Markdown: https://www.markdownguide.org/cheat-sheet
