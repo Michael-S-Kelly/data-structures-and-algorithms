5bdbce17381da2d879cb455fb42607cf7d9402ea
Michael Kelly <mike.s.kelly@icloud.com>
2019-04-11 11:44:41 -0700

    Got challenge 18 to pass the test

--- DataStructures/Tree/ReadMe.md
+++ DataStructures/Tree/ReadMe.md
@@ -20,12 +20,16 @@ This C# implementation creates a binary search tree and uses each of the three m
 | Add | Adds a new node to an existing Binary Search Tree. | O(logN) | O(1) | 2 |
 | Contains | Transverses through a Binary Search Tree to see if it contains a praticular value. | O(logN) | O(1) | 2 |
 | BreadthFirst | Create an array that shows the value order of a breadth-first search of the binary tree. | O(n) | O(n) | {1, 4, 5 } |
+| FindMaximumValue | Looks through the entire Binary Tree to find the Maximum Data Value. | O(n) | O(1) | 13 |
 
 ------------------------------
 
 ## Visuals
 ##### Whiteboard
 ![Image 1](Assets/Chall17Whiteboard.jpg)
+##### Whiteboard
+![Image 1](Assets/Chall18Whiteboard.jpg)
+
 ------------------------------
 
 ## Change Log
--- DataStructures/Tree/Tree/Classes/BinaryTree.cs
+++ DataStructures/Tree/Tree/Classes/BinaryTree.cs
@@ -83,6 +83,58 @@ namespace Tree.Classes
         }
 
         /// <summary>
+        /// Adds a new node to an existing Binary Tree
+        /// </summary>
+        /// <param name="data">Number that will go in the newly created node</param>
+        public void Insert(int data)
+        {
+            Node node = new Node(data);
+            Current = Root;
+            bool check = false;
+            while (!check)
+            {
+                if (Current.LChild == null)
+                {
+                    Current.LChild = node;
+                    check = true;
+                    return;
+                }
+                if (Current.RChild == null)
+                {
+                    Current.RChild = node;
+                    check = true;
+                    return;
+                }
+                if (Current.LChild.LChild == null || Current.LChild.RChild == null)
+                {
+                    Current = Current.LChild;
+                    check = false;
+                }
+                else if (Current.RChild.LChild == null || Current.RChild.RChild == null)
+                {
+                    Current = Current.RChild;
+                    check = false;
+                }
+                else if (Current.LChild.RChild.RChild == null)
+                {
+                    Current = Current.LChild;
+                    check = false;
+                }
+                else if (Current.RChild.RChild.RChild == null)
+                {
+                    Current = Current.RChild;
+                    check = false;
+                }
+                else
+                {
+                    Current = Current.LChild;
+                    check = false;
+                }
+            }
+            
+        }
+
+        /// <summary>
         /// Transverses through a Binary Search Tree to see if it contains a praticular value
         /// </summary>
         /// <param name="value">The target value that is being searched for</param>
@@ -153,5 +205,49 @@ namespace Tree.Classes
             }
             return results;
         }
+
+        #region Code Challenge 18 Maximum Value in a Binary Tree
+        /// <summary>
+        /// Looks through the entire Binary Tree to find the Maximum Data Value.
+        /// </summary>
+        /// <param name="tree">Binary Tree to be searched</param>
+        /// <returns>Maximum Data Value in the Binary Tree</returns>
+        public int FindMaximumValue(BinaryTree tree)
+        {
+            Current = Root;
+            int maxValue = Root.Data;
+
+            maxValue = UpdateMaxValue(tree, maxValue);
+
+            return maxValue;
+        }
+
+        /// <summary>
+        /// Using a recursive Method cycles through all the nodes in the Binary Tree and updates the maxValue.
+        /// </summary>
+        /// <param name="tree">Binary Tree to be searched</param>
+        /// <param name="maxValue">Last updated Maximum Data Value</param>
+        /// <returns>Newly updated Maximum Data Value</returns>
+        public int UpdateMaxValue(BinaryTree tree, int maxValue)
+        {
+            if (Current.Data > maxValue)
+            {
+                maxValue = Current.Data;
+            }
+            if (Current.LChild != null)
+            {
+                Current = Current.LChild;
+                maxValue = UpdateMaxValue(tree, maxValue);
+
+            }
+            if (Current.RChild != null)
+            {
+                Current = Current.RChild;
+                maxValue = UpdateMaxValue(tree, maxValue);
+            }
+
+            return maxValue;
+        }
+        #endregion
     }
 }
